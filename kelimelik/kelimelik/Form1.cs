e672cdb89c6670d41aa51a001fc1520380b5da40
Tayfun Yaltur <36271003+tayfunyaltur@users.noreply.github.com>
2019-04-05 17:52:27 +0300

    github sorunu cozme

--- kelimelik/kelimelik/Form1.cs
+++ kelimelik/kelimelik/Form1.cs
@@ -17,11 +17,11 @@ namespace kelimelik
         }
         
         public static int hangi_buton;
-
+        internal static Oyuncu o;
         private void Btn_YeniOyun_Click(object sender, EventArgs e)
         {
             Form2 f2 = new Form2();
-            Oyuncu o = new Oyuncu(textBox1.Text, f2);
+            o = new Oyuncu(textBox1.Text, f2);
             Oyun o2 = new Oyun(f2);
             o2.OyunBaslat();
             this.Hide();
